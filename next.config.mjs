f06c1538c21dcaa9eff149c68f686b856fa31d60
Marcin Szyda <marcinszyda25@gmail.com>
2024-04-09 12:03:29 +0200

    vercel fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/public': ['./public']
-    }
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
