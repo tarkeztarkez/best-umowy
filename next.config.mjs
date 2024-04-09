0501fefa351fe6d80569a6015453a252bf19a685
Marcin Szyda <marcinszyda25@gmail.com>
2024-04-09 12:19:40 +0200

    vercel fix 7

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./umowa.docx"]
+    }
+  }
+};
 
 export default nextConfig;
